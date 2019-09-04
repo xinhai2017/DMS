a93e52dfe3adb023884cde7dc10246b01a191186
HONG <1216648438@qq.com>
2019-09-04 17:10:54 +0800

    Update mutilpool_split_train_test.py

--- data_processing/delete_error_images/mutilpool_split_train_test.py
+++ data_processing/delete_error_images/mutilpool_split_train_test.py
@@ -8,10 +8,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 train_test_split_vatio = 0.2
 
-path_root = "../../../chenh_data/save_faces/"
+path_root = "./save_faces/"
 
 # 存储图片路径
-path_save_face = "../../../chenh_data/split_train_test_face/"
+path_save_face = "./split_train_test_face/"
 
 
 def data_load_fs(path_root, category, path_save_face):
@@ -34,7 +34,6 @@ def split_images(image_path, category, image_index, save_path):
         return img
 
 def split_train_test_faces(category, save_path,img_list):
-    # pass
 
     if not os.path.exists(os.path.join(save_path,"train")):
         os.makedirs(os.path.join(save_path,"train"))
@@ -44,9 +43,7 @@ def split_train_test_faces(category, save_path,img_list):
 
     test_img = img_list[: int(len(img_list) * train_test_split_vatio)]
     train_img = img_list[int(len(img_list) * train_test_split_vatio): ]
-    # print("%s test_img" %category, len(test_img))
-    # print("%s train_img" %category, len(train_img))
-
+ 
     for index,test_image in enumerate(test_img):
         cv2.imwrite("%s/test/%s.%d.jpg" % (save_path, category, index), test_image)
         # print("%s test image save finished!" %category)
@@ -67,21 +64,3 @@ result4 = pool.apply_async(data_load_fs,(path_root, "other", path_save_face))
 
 pool.close()
 pool.join()
-
-# data_load_fs(path_root,"smoke",path_save_face)
-
-# t1 = threading.Thread(target=data_load_fs, args=(path_root, "smoke", path_save_face))
-# t2 = threading.Thread(target=data_load_fs, args=(path_root, "drink", path_save_face))
-# t3 = threading.Thread(target=data_load_fs, args=(path_root, "phone", path_save_face))
-# t4 = threading.Thread(target=data_load_fs, args=(path_root, "other", path_save_face))
-#
-#
-# t1.start()
-# t2.start()
-# t3.start()
-# t4.start()
-#
-# t1.join()
-# t2.join()
-# t3.join()
-# t4.join()
